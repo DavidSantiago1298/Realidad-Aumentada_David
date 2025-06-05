db8e54edd8d0828bbdfcd7efa2a981781087a07d
77PaladinRata <9toxicshadow9@gmail.com>
2025-06-04 19:51:53 -0600

    EXAMEN_1P

--- tEST VUFORIA/Assets/Scripts/NotesChecker.cs
+++ tEST VUFORIA/Assets/Scripts/NotesChecker.cs
@@ -2,24 +2,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class NotesCheker : MonoBehaviour
+public class NotesChecker : MonoBehaviour
 {
     [SerializeField]
-
     private UnityEvent onButtonPressed;
     [SerializeField]
-
     private UnityEvent onCorrectNote;
     [SerializeField]
     private UnityEvent onFailNote;
     private List<GameObject> notes = new List<GameObject>();
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Note"))
+        {
             notes.Add(collision.gameObject);
+        }
     }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Note"))
@@ -27,24 +25,31 @@ public class NotesCheker : MonoBehaviour
             notes.Remove(collision.gameObject);
         }
     }
-
     public void DestroyNotes()
     {
-        if (notes.Count > 0)
+        onButtonPressed?.Invoke();
+        GameObject noteToDestroy = null;
+        int indexToRemove = -1;
+        for (int i = 0; i < notes.Count; i++)
+        {
+            if (notes[i] != null)
+            {
+                noteToDestroy = notes[i];
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        if (noteToDestroy != null)
         {
             onCorrectNote?.Invoke();
+            notes.RemoveAt(indexToRemove);
+            Destroy(noteToDestroy);
         }
         else
         {
             onFailNote?.Invoke();
         }
-        onButtonPressed?.Invoke();
-        while (notes.Count > 0)
-        {
-            GameObject note = notes[0];
-            notes.RemoveAt(0);
-            Destroy(note);
-        }
-        notes.Clear();
     }
+
 }
